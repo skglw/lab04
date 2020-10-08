c96280f5fdcd722936d7298fbe7b849f8941c1c8
skglw <skglvv@gmail.com>
2020-10-08 11:02:03 +0400

    first commmit

---
+++ lab04.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/lua5.3
+lgi = require 'lgi'
+gtk = lgi.Gtk
+pixbuf = lgi.GdkPixBuf.PixBuf
+gtk.init()
+bld = gtk.Builder()
+bld:add_from_file('lab04.glade')
+ui = bld.objects
+
+funtion ui.btnAdd:on_clicked(...)
+	name = ui.eName.text
+	value = tonumber(ui.eValue.text)
+	px = pixbuf.new_from_file('img-blueberry.png')
+	i = ui.items:append()
+	ui.items[i] = {[1] = name, [2] = value, [3] = px}
+end
+
+rdr_txt = gtk.CellrRendererText {}
+rdr_px = gtk.CellrRendererPixbuf {}
+
+c1 = gtk.TreeViewColumn { title = 'Name',  { rdr_txt, { text = 1   } } }
+c2 = gtk.TreeViewColumn { title = 'Value', { rdr_txt, { text = 2   } } }
+c3 = gtk.TreeViewColumn { title = 'Image', { rdr_px,  { pixbuf = 3 } } }
+
+ui.listItems:append_column(c1)
+ui.listItems:append_column(c2)
+ui.listItems:append_column(c3)
